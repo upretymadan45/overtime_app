1468131c6aa13e85c301c9090a4045ea8495b792
Madan <upretymadan45@gmail.com>
2016-11-27 15:01:29 +1300

    Implemented edit functionality for status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: 'MM', last_name: 'Uprety')
 puts "1 user created"
+
+AdminUser.create(email: "admin@admin.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: 'Admin', last_name: 'Uprety')
+puts "1 admin_user created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
