7704ab718ac29b39b117cd4cca14d812a7c1f2fc
Madan <upretymadan45@gmail.com>
2016-11-30 14:28:20 +1300

    Updated the audit log factory and tested out 6 day default

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
                     password_confirmation: "asdfasdf",
                     first_name: 'MM',
                     last_name: 'Uprety',
-                    phone: "55555")
+                    phone: "5555555555")
 puts "1 user created"
 
 AdminUser.create(email: "admin@admin.com",
@@ -21,3 +21,9 @@ puts "1 admin_user created"
                 overtime_request: 2.5)
 end
 puts "100 posts have been created"
+
+100.times do |audit_log|
+	AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "100 audit logs have been created"
