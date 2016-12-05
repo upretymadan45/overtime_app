96f83684cbea1ba60b40b68d6011b4e796c90b0e
Madan <upretymadan45@gmail.com>
2016-12-05 16:30:30 +1300

    Built out rake task for sending sms messages and STI for employee

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.create(email: "test@test.com",
+@employee = Employee.create(email: "test@test.com",
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
                     first_name: 'MM',
@@ -14,9 +14,9 @@ AdminUser.create(email: "admin@admin.com",
                   phone: "4444444444")
 puts "1 admin_user created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
@@ -28,7 +28,7 @@ puts "3 audit logs have been created"
                 irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
                 id est laborum.",
-                user_id: @user.id,
+                user_id: @employee.id,
                 overtime_request: 2.5)
 end
 puts "100 posts have been created"
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -2,9 +2,13 @@ namespace :notification do
   desc "sends sms notification to employee asking them to log if they had overtime or not"
   task sms: :environment do
     if Time.now.sunday?
+      employees = Employee.all
+      notification_message = "Please log into the overtime management dashboard to request overtime or confirm your hours for last week: https://mku-overtime.herokuapp.com"
 
+      employees.each do |employee|
+        SmsTool.send_sms(number: employee.phone, message: notification_message)
+      end
     end
-    puts "I am in the rake task"
   end
 
   desc "sends mail notification to managers(AdminUsers) each day to inform of pending overtime requests "
